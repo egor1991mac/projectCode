1c6b24790f889b901eb835d4c1ca21c5572e8156
Егор Егоров <egoregorov@MacBook-Pro-Egor.local>
2018-05-23 11:30:21 +0300

    new component

---
+++ modal/index.js
@@ -0,0 +1,43 @@
+import './style.scss';
+import template from './template.jade'
+
+export default class Modal {
+    constructor(elem,component){
+        this.elem = document.querySelector(elem);
+        this.component = component || null;
+        this.node = this.create_modal();
+        this.component_render();
+        this.open_modal();
+        this.close_modal();
+    }
+    create_modal(){
+        const node  = document.createElement('div');
+        node.id= "modal";
+        this.elem.appendChild(node);
+        return node;
+    }
+    component_render(){
+        if(this.component != null){
+            this.component(`#${this.node.id}`);
+        }
+        else{
+            this.node.innerHTML = template();
+        }
+    }
+    
+    open_modal(){
+        setTimeout(() => {
+            this.node.classList.toggle('open_modal');
+        }, 100);
+        
+    }
+    close_modal(){
+        this.node.onclick = function(event){
+            if(event.target === this){
+             this.classList.toggle('open_modal');
+             this.remove();
+            }
+         }
+    }
+}
+
